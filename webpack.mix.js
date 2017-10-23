a15e397a1aeeda9f7bbb2e4628f624fa6177db60
Brandon Nifong <brandon@tendency.me>
2017-10-23 18:01:12 -0400

    Add generic docblocks to our current scss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,6 @@
-const mix = require('laravel-mix');
+const mix            = require('laravel-mix');
+const PrettierPlugin = require("prettier-webpack-plugin");
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 /*
  |--------------------------------------------------------------------------
@@ -19,11 +21,25 @@ mix.setPublicPath(dist)
    .setResourceRoot('../');
    
 // Assets
-mix.sass(`${assets}/styles/app.scss`, `${dist}/styles`)
+mix.sass(`${assets}/styles/main.scss`, `${dist}/styles`)
    .js(`${assets}/scripts/main.js`, `${dist}/scripts`)
    .extract([
      'micromodal/dist/micromodal'
    ]);
+   
+// Plugins 
+mix.webpackConfig({
+  plugins: [
+    new ImageminPlugin({
+      pngquant: {
+        quality: '95-100'
+      },
+      test: /\.(jpe?g|png|gif|svg)$/i
+    }),
+    
+    new PrettierPlugin()
+  ]
+});
 
 // Source maps when not in production.
 if (!mix.inProduction()) {
